2be6a532e80f4c6a08b7af41616a8fd69dc05974
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-13 03:18:56 +0300

    ch: register api projects serializer

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'projects', views.ProjectViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
