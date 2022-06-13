ada706bdbddc27cd7c4c5acaf109354feb8e31ab
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-13 03:18:04 +0300

    create api models-- user profile ++ projects model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,27 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+from django.contrib.auth.models import User
+
+
+# todo: create user profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile/', blank=True)
+    bio = models.TextField('user bio')
+
+    def __str__(self):
+        return self.user.username
 
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, default='project')
+    image_file = models.ImageField(upload_to='images/', blank=True)
+    description = models.TextField('project description')
+    url = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # model data display representation
+    def __str__(self):
+        return self.name
