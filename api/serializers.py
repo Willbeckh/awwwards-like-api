6a1d62de183eaa659051b8232ee6567d88aa5976
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-13 03:18:30 +0300

    add project model serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,25 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+# local imports
+from api.models import Project
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'url', 'username', 'email', 'groups']
-        
-    
+
+
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+# projects object repr serializer
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'author', 'name', 'image_file',
+                  'description', 'url', 'created_at']
