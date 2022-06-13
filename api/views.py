f52a573c5f18177d4f71ce3b6f4ba821c8a5bf2b
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-13 03:19:14 +0300

    Add a projects viewset

--- api/views.py
+++ api/views.py
@@ -2,7 +2,10 @@ from email.headerregistry import Group
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
-from api.serializers import UserSerializer, GroupSerializer
+from api.serializers import UserSerializer, GroupSerializer, ProjectSerializer
+
+# local imports
+from api.models import Project
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,3 +25,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
         
+        
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for viewing and editing user projects
+    """
+    queryset = Project.objects.all().order_by('-created_at')
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
