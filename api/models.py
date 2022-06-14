0b4e366af2b49c94bc0c0ed0417a62c5f7b2d249
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-14 19:20:10 +0300

    create user projectss relation field

--- api/models.py
+++ api/models.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 
 # todo: create user profile model
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
     profile_pic = models.ImageField(upload_to='profile/', blank=True)
     bio = models.TextField('user bio')
+    projects = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='projects', blank=True, null=True)
 
     def __str__(self):
         return self.user.username
 
 
 # Create your models here.
-
 class Project(models.Model):
     name = models.CharField(max_length=200, default='project')
     image_file = models.ImageField(upload_to='images/', blank=True)
