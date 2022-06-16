a22cb1971901dd389410825a3192dc69317f028d
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-16 09:57:36 +0300

    update app urls conf

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_swagger.views import get_swagger_view
 
 # local app imports
 from . import views
 from .views import MyObtainTokenPairView, RegisterView, RatingViewSet
 
 
+# schema_view = get_swagger_view(title='Awards API')
+
 # setting up api routers
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 router.register(r'projects', views.ProjectViewSet)
 router.register(r'ratings', views.RatingViewSet)
 
