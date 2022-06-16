b2950bc3993003bb44f0872a0bf14b4741ff9976
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-16 10:00:34 +0300

    add swagger url config

--- awards/urls.py
+++ awards/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
+    path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html',
+         extra_context={'schema_url': 'openapi-schema'}), name='schema-ui'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
