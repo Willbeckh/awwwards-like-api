8e71c5dbd8d3b3a31b7b0d579261c933c525dee1
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-22 19:32:55 +0300

    structure app urls routes

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_swagger.views import get_swagger_view
 
 # local app imports
 from . import views
-from .views import MyObtainTokenPairView, RegisterView, RatingViewSet
+from .views import MyObtainTokenPairView, RegisterView, RatingViewSet, HomeView
 
 
 # schema_view = get_swagger_view(title='Awards API')
@@ -14,12 +14,15 @@ from .views import MyObtainTokenPairView, RegisterView, RatingViewSet
 router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet)
 router.register(r'projects', views.ProjectViewSet)
-# router.register(r'ratings', views.RatingViewSet)
 
 urlpatterns = [
+    # api endpoints
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    
+    # app endpoints
+    path('', HomeView.as_view(), name='home')
 ]
