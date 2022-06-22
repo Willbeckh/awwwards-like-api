d949f61660612446e46e928eabcd856ba5534207
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-22 21:44:17 +0300

    pt: remove app code

--- api/urls.py
+++ api/urls.py
@@ -1,29 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
-from rest_framework_swagger.views import get_swagger_view
 
 # local app imports
 from . import views
-from .views import MyObtainTokenPairView, RegisterView, RatingViewSet, HomeView, ProjectView
+from .views import MyObtainTokenPairView, RegisterView
 
-
-# schema_view = get_swagger_view(title='Awards API')
 
 # setting up api routers
 router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
+router.register(r'users', views.UserViewSet)
 router.register(r'projects', views.ProjectViewSet)
 
 urlpatterns = [
     # api endpoints
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
-    
-    # app endpoints
-    path('', HomeView.as_view(), name='home'),
-    path('project/<int:id>', ProjectView.as_view(), name='project')
 ]
